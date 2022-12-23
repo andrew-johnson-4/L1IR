eb13adee5a3f2ed2e2b2c305c84d76b5d02826e1
Andrew Johnson <andrew@subarctic.org>
2022-12-22 22:23:30 -0700

    Decorate AST with Span:Debug

--- src/eval.rs
+++ src/eval.rs
@@ -1,10 +1,11 @@
 use std::rc::Rc;
+use std::fmt::Debug;
 use std::collections::{HashMap};
 use crate::ast::{Value,Expression,Program,LIPart,TIPart};
 
-pub fn eval_e(lctx: &mut HashMap<usize,Value>, pctx: &Program, e: &Expression) -> Value {
+pub fn eval_e<S:Debug>(lctx: &mut HashMap<usize,Value>, pctx: &Program<S>, e: &Expression<S>) -> Value {
    match e {
-      Expression::LiteralIntroduction(lps) => {
+      Expression::LiteralIntroduction(lps,span) => {
          if lps.len()==1 {
          if let LIPart::Linear(lcs) = &lps[0] {
             return Value::Literal(0,lcs.len(),lcs.clone());
@@ -20,12 +21,12 @@ pub fn eval_e(lctx: &mut HashMap<usize,Value>, pctx: &Program, e: &Expression) -
                if let Some(Value::Literal(vs,ve,vcs)) = lctx.get(vi) {
                for ci in *vs..*ve {
                   lcs.push(vcs[ci]);
-               }} else { panic!("v#{} not found", vi) }
+               }} else { panic!("v#{} not found at {:?}", vi, span) }
             },
          }}
          Value::Literal(0,lcs.len(),Rc::new(lcs))
       },
-      Expression::TupleIntroduction(tps) => {
+      Expression::TupleIntroduction(tps,span) => {
          if tps.len()==1 {
          if let TIPart::Linear(tcs) = &tps[0] {
             return Value::Tuple(0,tcs.len(),tcs.clone());
@@ -40,28 +41,28 @@ pub fn eval_e(lctx: &mut HashMap<usize,Value>, pctx: &Program, e: &Expression) -
             TIPart::Variable(vi) => {
                if let Some(vt) = lctx.get(vi) {
                   tcs.push(vt.clone());
-               } else { panic!("v#{} not found", vi) }
+               } else { panic!("v#{} not found at {:?}", vi, span) }
             },
             TIPart::InlineVariable(vi) => {
                if let Some(Value::Tuple(vs,ve,vcs)) = lctx.get(vi) {
                for ti in *vs..*ve {
                   tcs.push(vcs[ti].clone());
-               }} else { panic!("inline tuple v#{} not found", vi) }
+               }} else { panic!("inline tuple v#{} not found at {:?}", vi, span) }
             },
          }}
          Value::Tuple(0,tcs.len(),Rc::new(tcs))
       },
-      Expression::FunctionReference(fi) => {
+      Expression::FunctionReference(fi,_span) => {
          Value::Function(*fi)
       },
-      Expression::VariableReference(li) => {
+      Expression::VariableReference(li,span) => {
          if let Some(lv) = lctx.get(li) {
             lv.clone()
-         } else { panic!("eval_e(v#{})", li) }
+         } else { panic!("v#{} not found at {:?}", li, span) }
       },
-      Expression::FunctionApplication(fi,pxs) => {
-         if *fi>pctx.functions.len() { panic!("f#{} undefined", fi); }
-         if pxs.len()!=pctx.functions[*fi].args.len() { panic!("f#{} call with wrong arity", fi); }
+      Expression::FunctionApplication(fi,pxs,span) => {
+         if *fi>pctx.functions.len() { panic!("f#{} undefined at {:?}", fi, span); }
+         if pxs.len()!=pctx.functions[*fi].args.len() { panic!("f#{} call with wrong arity at {:?}", fi, span); }
          let mut ps = Vec::new();
          for px in pxs.iter() {
             ps.push(eval_e(lctx, pctx, px));
@@ -76,12 +77,12 @@ pub fn eval_e(lctx: &mut HashMap<usize,Value>, pctx: &Program, e: &Expression) -
          }
          top_value
       },
-      Expression::PatternMatch => unimplemented!("eval_e()"),
-      Expression::Failure => panic!("eval_e(Failure)"),
+      Expression::PatternMatch(span) => unimplemented!("eval_e(PatternMatch) at {:?}", span),
+      Expression::Failure(span) => panic!("eval_e(Failure) at {:?}", span),
    }
 }
 
-pub fn eval(p: Program) -> Value {
+pub fn eval<S:Debug>(p: Program<S>) -> Value {
    let mut top_value = Value::tuple(Vec::new());
    let mut top_ctx = HashMap::new();
    for e in p.expressions.iter() {
