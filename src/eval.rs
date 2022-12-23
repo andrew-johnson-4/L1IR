c4ec79d4d11e88261d84918eba469d191c099362
Andrew Johnson <andrew@subarctic.org>
2022-12-22 18:46:06 -0700

    start evaluating expressions

--- src/eval.rs
+++ src/eval.rs
@@ -1,7 +1,22 @@
+use std::collections::{HashMap};
+use crate::ast::{Value,Expression,Program};
 
-use crate::ast::{Value,Program};
+pub fn eval_e(_lctx: &mut HashMap<usize,Value>, _pctx: &Program, e: &Expression) -> Value {
+   match e {
+      Expression::LiteralIntroduction(_lps) => unimplemented!("eval_e(LiteralIntroduction)"),
+      Expression::TupleIntroduction(_tps) => unimplemented!("eval_e(TupleIntroduction)"),
+      Expression::VariableReference(_l) => unimplemented!("eval_e(VariableReference)"),
+      Expression::FunctionApplication(_fx,_pxs) => unimplemented!("eval_e(FunctionApplication)"),
+      Expression::PatternMatch => unimplemented!("eval_e()"),
+      Expression::Failure => panic!("eval_e(Failure)"),
+   }
+}
 
-pub fn eval(_p: Program) -> Value {
-   let top_value = Value::tuple(Vec::new());
+pub fn eval(p: Program) -> Value {
+   let mut top_value = Value::tuple(Vec::new());
+   let mut top_ctx = HashMap::new();
+   for e in p.expressions.iter() {
+      top_value = eval_e(&mut top_ctx, &p, e);
+   }
    top_value
 }
