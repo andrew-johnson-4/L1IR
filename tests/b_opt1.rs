187160ff418e2cd4a3359c04267c7c6e28cb38a3
Andrew Johnson <andrew@subarctic.org>
2022-12-28 22:24:48 -0700

    setup a test harness for JIT compilation

---
+++ tests/b_opt1.rs
@@ -0,0 +1,12 @@
+//use l1_ir::ast::{Expression,Program,FunctionDefinition,LHSPart,LHSLiteralPart,LIPart};
+//use l1_ir::eval::{eval};
+
+#[test]
+fn eval_add() {
+   for _x in 0..20 {
+   for _y in 0..20 {
+      let _nojit = 0;
+      let _jit = 0;
+      //assert eval(nojit) = eval(jit)
+   }}
+}
