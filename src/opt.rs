facf35acbe4b14299e6d8ff95ee4ddd90cceccc3
Andrew Johnson <andrew@subarctic.org>
2022-12-31 21:11:08 -0700

    start migrating L1 types to cranelift Types

--- src/opt.rs
+++ src/opt.rs
@@ -4,40 +4,58 @@ use cranelift::prelude::*;
 use cranelift_jit::{JITBuilder, JITModule};
 use cranelift_module::{DataContext, Linkage, Module};
 
-pub struct JITProgram {
+pub struct JProgram {
     builder_context: FunctionBuilderContext,
     ctx: codegen::Context,
     data_ctx: DataContext,
     module: JITModule,
 }
 
-impl JITProgram {
-   pub fn compile_fn<S: Clone + Debug>(&mut self) {
-      //functions will not be compiled until referenced
+pub struct JType {
+   pub name: String,
+   pub jtype: types::Type,
+}
+
+impl JProgram {
+   //functions will not be compiled until referenced
+   pub fn compile_fn<S: Clone + Debug>(&mut self, p: &Program<S>, fi: usize, args: Vec<JType>) -> JType {
+      unimplemented!("compile function: f#{}", fi)
    }
-   pub fn compile_expr<S: Clone + Debug>(&mut self, e: &Expression<S>) {
+   pub fn compile_expr<S: Clone + Debug>(&mut self, p: &Program<S>, e: &Expression<S>) -> JType {
       match e {
-         Expression::UnaryIntroduction(_ui,_span) => unimplemented!("compile expression: UnaryIntroduction"),
+         Expression::UnaryIntroduction(_ui,_span) => {
+            JType {
+               name: "Unary".to_string(),
+               jtype: types::I64,
+            }
+         },
          Expression::LiteralIntroduction(_li,_span) => unimplemented!("compile expression: LiteralIntroduction"),
          Expression::TupleIntroduction(_ti,_span) => unimplemented!("compile expression: TupleIntroduction"),
          Expression::VariableReference(_vi,_span) => unimplemented!("compile expression: VariableReference"),
          Expression::FunctionReference(_vi,_span) => unimplemented!("compile expression: FunctionReference"),
-         Expression::FunctionApplication(_fi,_args,_span) => unimplemented!("compile expression: FunctionApplication"),
+         Expression::FunctionApplication(fi,args,_span) => {
+            let mut arg_types = Vec::new();
+            for a in args.iter() {
+               let jt = self.compile_expr(p, a);
+               arg_types.push(jt);
+            }
+            self.compile_fn(p, *fi, arg_types)
+         },
          Expression::PatternMatch(_pe,_lrs,_span) => unimplemented!("compile expression: PatternMatch"),
          Expression::Failure(_span) => unimplemented!("compile expression: Failure"),
       }
    }
    pub fn compile<S: Clone + Debug>(&mut self, p: &Program<S>) {
       for pe in p.expressions.iter() {
-         self.compile_expr(pe);
+         self.compile_expr(p, pe);
       }
    }
 }
 
-pub fn jsweep<S: Clone + Debug>(p: Program<S>) -> JITProgram {
+pub fn jsweep<S: Clone + Debug>(p: Program<S>) -> JProgram {
    let builder = JITBuilder::new(cranelift_module::default_libcall_names());
    let module = JITModule::new(builder.unwrap());
-   let mut jp = JITProgram {
+   let mut jp = JProgram {
       builder_context: FunctionBuilderContext::new(),
       ctx: module.make_context(),
       data_ctx: DataContext::new(),
@@ -47,7 +65,7 @@ pub fn jsweep<S: Clone + Debug>(p: Program<S>) -> JITProgram {
    jp
 }
 
-impl JITProgram {
+impl JProgram {
    pub fn eval(&self) -> Result<Value,Error<String>> {
       Ok(Value::unary(b"00"))
    }
