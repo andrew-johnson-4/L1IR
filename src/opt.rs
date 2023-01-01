1f88618f485cb1d3e4de7bf2ff1d17fd9ab316af
Andrew Johnson <andrew@subarctic.org>
2022-12-31 21:46:44 -0700

    preamble for main function

--- src/opt.rs
+++ src/opt.rs
@@ -1,8 +1,9 @@
 use std::fmt::Debug;
-use crate::ast::{Program,Value,Error,Expression};
+use crate::ast;
+use crate::ast::{Program,Error,Expression};
 use cranelift::prelude::*;
 use cranelift_jit::{JITBuilder, JITModule};
-use cranelift_module::{DataContext, Linkage, Module};
+use cranelift_module::{DataContext, Module};
 
 pub struct JProgram {
     builder_context: FunctionBuilderContext,
@@ -11,43 +12,59 @@ pub struct JProgram {
     module: JITModule,
 }
 
+pub struct JExpr {
+   pub value: Value,
+}
+
 pub struct JType {
    pub name: String,
    pub jtype: types::Type,
 }
 
+pub fn compile_expr<'f,S: Clone + Debug>(p: &Program<S>, f: &mut FunctionBuilder<'f>, e: &Expression<S>) -> (JExpr,JType) {
+   match e {
+      Expression::UnaryIntroduction(_ui,_span) => {
+         (JExpr {
+            value: f.ins().iconst(types::I64, i64::from(0))
+         }, JType {
+            name: "Unary".to_string(),
+            jtype: types::I64,
+         })
+      },
+      Expression::LiteralIntroduction(_li,_span) => unimplemented!("compile expression: LiteralIntroduction"),
+      Expression::TupleIntroduction(_ti,_span) => unimplemented!("compile expression: TupleIntroduction"),
+      Expression::VariableReference(_vi,_span) => unimplemented!("compile expression: VariableReference"),
+      Expression::FunctionReference(_vi,_span) => unimplemented!("compile expression: FunctionReference"),
+      Expression::FunctionApplication(fi,args,_span) => {
+         let mut arg_types = Vec::new();
+         for a in args.iter() {
+            let (je,jt) = compile_expr(p, f, a);
+            arg_types.push(jt);
+         }
+         apply_fn(p, *fi, arg_types)
+      },
+      Expression::PatternMatch(_pe,_lrs,_span) => unimplemented!("compile expression: PatternMatch"),
+      Expression::Failure(_span) => unimplemented!("compile expression: Failure"),
+   }
+}
+
+pub fn apply_fn<S: Clone + Debug>(p: &Program<S>, fi: usize, args: Vec<JType>) -> (JExpr,JType) {
+   unimplemented!("apply function: f#{}", fi)
+}
+
 impl JProgram {
    //functions will not be compiled until referenced
-   pub fn compile_fn<S: Clone + Debug>(&mut self, p: &Program<S>, fi: usize, args: Vec<JType>) -> JType {
-      unimplemented!("compile function: f#{}", fi)
-   }
-   pub fn compile_expr<S: Clone + Debug>(&mut self, p: &Program<S>, e: &Expression<S>) -> JType {
-      match e {
-         Expression::UnaryIntroduction(_ui,_span) => {
-            JType {
-               name: "Unary".to_string(),
-               jtype: types::I64,
-            }
-         },
-         Expression::LiteralIntroduction(_li,_span) => unimplemented!("compile expression: LiteralIntroduction"),
-         Expression::TupleIntroduction(_ti,_span) => unimplemented!("compile expression: TupleIntroduction"),
-         Expression::VariableReference(_vi,_span) => unimplemented!("compile expression: VariableReference"),
-         Expression::FunctionReference(_vi,_span) => unimplemented!("compile expression: FunctionReference"),
-         Expression::FunctionApplication(fi,args,_span) => {
-            let mut arg_types = Vec::new();
-            for a in args.iter() {
-               let jt = self.compile_expr(p, a);
-               arg_types.push(jt);
-            }
-            self.compile_fn(p, *fi, arg_types)
-         },
-         Expression::PatternMatch(_pe,_lrs,_span) => unimplemented!("compile expression: PatternMatch"),
-         Expression::Failure(_span) => unimplemented!("compile expression: Failure"),
-      }
-   }
    pub fn compile<S: Clone + Debug>(&mut self, p: &Program<S>) {
+      let mut main = FunctionBuilder::new(&mut self.ctx.func, &mut self.builder_context);
+
+      //int main();
+      let entry_block = main.create_block();
+      main.append_block_params_for_function_params(entry_block);
+      main.switch_to_block(entry_block);
+      main.seal_block(entry_block);
+
       for pe in p.expressions.iter() {
-         self.compile_expr(p, pe);
+         compile_expr(p, &mut main, pe);
       }
    }
 }
@@ -66,7 +83,7 @@ pub fn jsweep<S: Clone + Debug>(p: Program<S>) -> JProgram {
 }
 
 impl JProgram {
-   pub fn eval(&self) -> Result<Value,Error<String>> {
-      Ok(Value::unary(b"00"))
+   pub fn eval(&self) -> Result<ast::Value,Error<String>> {
+      Ok(ast::Value::unary(b"00"))
    }
 }
