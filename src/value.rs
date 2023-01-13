4d5f18f10f204112b285e3e5b6797a6ec92ff727
Andrew Johnson <andrew@subarctic.org>
2023-01-12 22:03:36 -0700

    lots of Value stuff working

--- src/value.rs
+++ src/value.rs
@@ -28,6 +28,7 @@
 
 use num_derive::FromPrimitive;    
 use num_traits::FromPrimitive;
+use std::sync::Mutex;
 
 #[derive(FromPrimitive)]
 #[derive(Debug)]
@@ -36,7 +37,17 @@ pub enum Tag {
    Unit,
    I8, I82, I83, I84, I85, I86, I87, I88, I89, I810, I811, I812,
    U8, U82, U83, U84, U85, U86, U87, U88, U89, U810, U811, U812,
+   U16,
+   I16,
+   U32,
+   I32,
+   F32,
+   U64,
+   I64,
+   F64,
 }
+
+static NAMES: Mutex<Vec<String>> = Mutex::new(Vec::new());
 
 pub struct Value(u128);
 
@@ -44,62 +55,117 @@ impl Value {
    pub fn from_parts(tag: u16, name: u16, slots: u128) -> Value {
       let tag = (tag as u128) << 112;
       let name = (name as u128) << 96;
-      Value(tag + name + slots)
+      Value(tag | name | slots)
+   }
+   pub fn push_name(nom: &str) -> u16 {
+      let mut ns = NAMES.lock().unwrap();
+      for (ni,n) in ns.iter().enumerate() {
+      if n == nom {
+         return ni as u16;
+      }}
+      let ni = ns.len();
+      ns.push(nom.to_string());
+      ni as u16
    }
    pub fn unit(nom: &str) -> Value {
-      Value::from_parts(Tag::Unit as u16, 0, 0)
+      Value::from_parts(Tag::Unit as u16, Value::push_name(nom), 0)
    }
    pub fn i8(slot: i8, nom: &str) -> Value {
-      Value::from_parts(Tag::I8 as u16, 0, 0)
+      Value::from_parts(Tag::I8 as u16, Value::push_name(nom), (slot as u16) as u128)
    }
    pub fn u8(slot: u8, nom: &str) -> Value {
-      Value::from_parts(Tag::U8 as u16, 0, 0)
+      Value::from_parts(Tag::U8 as u16, Value::push_name(nom), (slot as u16) as u128)
    }
    pub fn i8s(slots: &[i8], nom: &str) -> Value {
+      let mut v: u128 = 0;
+      unsafe {
+         if slots.len()>=12 { v += std::mem::transmute::<i8,u8>(slots[11]) as u128; } v <<= 8;
+         if slots.len()>=11 { v += std::mem::transmute::<i8,u8>(slots[10]) as u128; } v <<= 8;
+         if slots.len()>=10 { v += std::mem::transmute::<i8,u8>(slots[9])  as u128; } v <<= 8;
+         if slots.len()>=9  { v += std::mem::transmute::<i8,u8>(slots[8])  as u128; } v <<= 8;
+         if slots.len()>=8  { v += std::mem::transmute::<i8,u8>(slots[7])  as u128; } v <<= 8;
+         if slots.len()>=7  { v += std::mem::transmute::<i8,u8>(slots[6])  as u128; } v <<= 8;
+         if slots.len()>=6  { v += std::mem::transmute::<i8,u8>(slots[5])  as u128; } v <<= 8;
+         if slots.len()>=5  { v += std::mem::transmute::<i8,u8>(slots[4])  as u128; } v <<= 8;
+         if slots.len()>=4  { v += std::mem::transmute::<i8,u8>(slots[3])  as u128; } v <<= 8;
+         if slots.len()>=3  { v += std::mem::transmute::<i8,u8>(slots[2])  as u128; } v <<= 8;
+         if slots.len()>=2  { v += std::mem::transmute::<i8,u8>(slots[1])  as u128; } v <<= 8;
+         if slots.len()>=1  { v += std::mem::transmute::<i8,u8>(slots[0])  as u128; } v <<= 8;
+      }
       match slots.len() {
-         0 => Value::from_parts(Tag::Unit as u16, 0, 0),
-         1 => Value::from_parts(Tag::I8 as u16, 0, 0),
-         2 => Value::from_parts(Tag::I82 as u16, 0, 0),
-         3 => Value::from_parts(Tag::I83 as u16, 0, 0),
-         4 => Value::from_parts(Tag::I84 as u16, 0, 0),
-         5 => Value::from_parts(Tag::I85 as u16, 0, 0),
-         6 => Value::from_parts(Tag::I86 as u16, 0, 0),
-         7 => Value::from_parts(Tag::I87 as u16, 0, 0),
-         8 => Value::from_parts(Tag::I88 as u16, 0, 0),
-         9 => Value::from_parts(Tag::I89 as u16, 0, 0),
-         10 => Value::from_parts(Tag::I810 as u16, 0, 0),
-         11 => Value::from_parts(Tag::I811 as u16, 0, 0),
-         12 => Value::from_parts(Tag::I812 as u16, 0, 0),
+         0 => Value::from_parts(Tag::Unit as u16, Value::push_name(nom), v),
+         1 => Value::from_parts(Tag::I8 as u16, Value::push_name(nom), v),
+         2 => Value::from_parts(Tag::I82 as u16, Value::push_name(nom), v),
+         3 => Value::from_parts(Tag::I83 as u16, Value::push_name(nom), v),
+         4 => Value::from_parts(Tag::I84 as u16, Value::push_name(nom), v),
+         5 => Value::from_parts(Tag::I85 as u16, Value::push_name(nom), v),
+         6 => Value::from_parts(Tag::I86 as u16, Value::push_name(nom), v),
+         7 => Value::from_parts(Tag::I87 as u16, Value::push_name(nom), v),
+         8 => Value::from_parts(Tag::I88 as u16, Value::push_name(nom), v),
+         9 => Value::from_parts(Tag::I89 as u16, Value::push_name(nom), v),
+         10 => Value::from_parts(Tag::I810 as u16, Value::push_name(nom), v),
+         11 => Value::from_parts(Tag::I811 as u16, Value::push_name(nom), v),
+         12 => Value::from_parts(Tag::I812 as u16, Value::push_name(nom), v),
          _ => unreachable!(),
       }
    }
    pub fn u8s(slots: &[u8], nom: &str) -> Value {
+      let mut v: u128 = 0;
+      if slots.len()>=12 { v += slots[11] as u128; } v <<= 8;
+      if slots.len()>=11 { v += slots[10] as u128; } v <<= 8;
+      if slots.len()>=10 { v += slots[9]  as u128; } v <<= 8;
+      if slots.len()>=9  { v += slots[8]  as u128; } v <<= 8;
+      if slots.len()>=8  { v += slots[7]  as u128; } v <<= 8;
+      if slots.len()>=7  { v += slots[6]  as u128; } v <<= 8;
+      if slots.len()>=6  { v += slots[5]  as u128; } v <<= 8;
+      if slots.len()>=5  { v += slots[4]  as u128; } v <<= 8;
+      if slots.len()>=4  { v += slots[3]  as u128; } v <<= 8;
+      if slots.len()>=3  { v += slots[2]  as u128; } v <<= 8;
+      if slots.len()>=2  { v += slots[1]  as u128; } v <<= 8;
+      if slots.len()>=1  { v += slots[0]  as u128; } v <<= 8;
       match slots.len() {
-         0 => Value::from_parts(Tag::Unit as u16, 0, 0),
-         1 => Value::from_parts(Tag::U8 as u16, 0, 0),
-         2 => Value::from_parts(Tag::U82 as u16, 0, 0),
-         3 => Value::from_parts(Tag::U83 as u16, 0, 0),
-         4 => Value::from_parts(Tag::U84 as u16, 0, 0),
-         5 => Value::from_parts(Tag::U85 as u16, 0, 0),
-         6 => Value::from_parts(Tag::U86 as u16, 0, 0),
-         7 => Value::from_parts(Tag::U87 as u16, 0, 0),
-         8 => Value::from_parts(Tag::U88 as u16, 0, 0),
-         9 => Value::from_parts(Tag::U89 as u16, 0, 0),
-         10 => Value::from_parts(Tag::U810 as u16, 0, 0),
-         11 => Value::from_parts(Tag::U811 as u16, 0, 0),
-         12 => Value::from_parts(Tag::U812 as u16, 0, 0),
+         0 => Value::from_parts(Tag::Unit as u16, Value::push_name(nom), v),
+         1 => Value::from_parts(Tag::U8 as u16, Value::push_name(nom), v),
+         2 => Value::from_parts(Tag::U82 as u16, Value::push_name(nom), v),
+         3 => Value::from_parts(Tag::U83 as u16, Value::push_name(nom), v),
+         4 => Value::from_parts(Tag::U84 as u16, Value::push_name(nom), v),
+         5 => Value::from_parts(Tag::U85 as u16, Value::push_name(nom), v),
+         6 => Value::from_parts(Tag::U86 as u16, Value::push_name(nom), v),
+         7 => Value::from_parts(Tag::U87 as u16, Value::push_name(nom), v),
+         8 => Value::from_parts(Tag::U88 as u16, Value::push_name(nom), v),
+         9 => Value::from_parts(Tag::U89 as u16, Value::push_name(nom), v),
+         10 => Value::from_parts(Tag::U810 as u16, Value::push_name(nom), v),
+         11 => Value::from_parts(Tag::U811 as u16, Value::push_name(nom), v),
+         12 => Value::from_parts(Tag::U812 as u16, Value::push_name(nom), v),
          _ => unreachable!(),
       }
    }
    pub fn tag<'t>(&self) -> String {
       let t = (self.0 >> 112) as u16;
-      let t: Tag = FromPrimitive::from_i32(t.into()).expect("Invalid Tag in Value");
+      let t: Tag = FromPrimitive::from_i32(t.into()).expect(&format!("Invalid Tag in Value: {}", t));
       format!("{:?}", t)
    }
    pub fn name(&self) -> String {
-      format!("Type#123")
+      let ni = ((self.0 << 16) >> 112) as usize;
+      let ns = NAMES.lock().unwrap();
+      ns[ni].clone()
    }
-   pub fn slot(&self, slot: usize) -> i128 {
-      0
+   pub fn slot(&self, tag: Tag, slot: usize) -> i128 {
+      let mut s = ((self.0 << 32) >> 32) as u128;
+      match tag {
+         Tag::Unit => { s = 0; },
+         Tag::U8|Tag::U82|Tag::U83|Tag::U84|Tag::U85|Tag::U86|Tag::U87|Tag::U88|Tag::U89|Tag::U810|Tag::U811|Tag::U812 => {
+            s <<= 32 + 8 * (11 - slot);
+            s >>= 32 + 8 * 11;
+         },
+         Tag::I8|Tag::I82|Tag::I83|Tag::I84|Tag::I85|Tag::I86|Tag::I87|Tag::I88|Tag::I89|Tag::I810|Tag::I811|Tag::I812 => {
+            s <<= 32 + 8 * (11 - slot);
+            s >>= 32 + 8 * 11;
+            let sv = s as u8;
+            s = unsafe { std::mem::transmute::<u8,i8>(sv) } as u128;
+         },
+         _ => unimplemented!("transmute slot {:?}", tag)
+      }
+      unsafe { std::mem::transmute::<u128,i128>(s) }
    }
 }
--- tests/a_values.rs
+++ tests/a_values.rs
@@ -1,4 +1,4 @@
-use l1_ir::value::{Value};
+use l1_ir::value::{Value,Tag};
 
 #[test]
 fn value_nil() {
@@ -12,7 +12,12 @@ fn value_number_i8() {
    let v = Value::i8(-2, "T#1");
    assert_eq!(v.tag(), "I8");
    assert_eq!(v.name(), "T#1");
-   assert_eq!(v.slot(0), -2);
+   assert_eq!(v.slot(Tag::I8, 0), -2);
+
+   let v = Value::i8(100, "T#3");
+   assert_eq!(v.tag(), "I8");
+   assert_eq!(v.name(), "T#3");
+   assert_eq!(v.slot(Tag::I8, 0), 100);
 }
 
 #[test]
@@ -20,5 +25,10 @@ fn value_number_u8() {
    let v = Value::u8(12, "T#3");
    assert_eq!(v.tag(), "U8");
    assert_eq!(v.name(), "T#3");
-   assert_eq!(v.slot(0), 12);
+   assert_eq!(v.slot(Tag::U8, 0), 12);
+
+   let v = Value::u8(9, "T#4");
+   assert_eq!(v.tag(), "U8");
+   assert_eq!(v.name(), "T#4");
+   assert_eq!(v.slot(Tag::U8, 0), 9);
 }
